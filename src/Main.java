ea273d4f177c08c1da5f4b642a8e1f450825a5c9
Nitix <guillaume.pierson.gp@gmail.com>
2015-04-03 18:04:02 +0200

    Ajout des algorithmes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -11,27 +12,76 @@ public class Main {
 		Point p2;
 		Rectangle rec;
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Entrer le nombre de rectangles � g�nerer ");
+		System.out.print("Entrer le nombre de rectangles à génerer ");
 		int n = sc.nextInt();
 		ArrayList<Rectangle> map = new ArrayList<Rectangle>();
 
 		System.out.print("Pour " + n + " rectangles: \n");
 
 		for (int i = 0; i < n; i++) {
-			x1 = (int) (Math.random() * 100);
-			x2 = (int) (Math.random() * x1);
-			y1 = (int) (Math.random() * 100);
-			y2 = (int) (Math.random() * y1);
+			x1 = (int) (Math.random() * 10000);
+			x2 = (int) ( x1 + 40); //HACK
+			y1 = (int) (Math.random() * 10000);
+			y2 = (int) (y1 + 40);
 
 			p1 = new Point(x1, y1);
 			p2 = new Point(x2, y2);
 			rec = new Rectangle(p1, p2);
+			//System.out.println("Rectangle " + i +" : P1(" + x1 + " , " + y1 + "), P2(" + x2 + " , " + y2 + ")" );
 			map.add(rec);
 
 		}
+		System.out.println("Les " + map.size() + " ont été générés");
 
-		System.out.println("Les " + map.size() + " ont �t� g�n�r�");
-
+		long time = System.currentTimeMillis();
+		pairAPair(map);
+		System.out.println(System.currentTimeMillis() - time + " ms");
+		time = System.currentTimeMillis();
+		balayage(map);
+		System.out.println(System.currentTimeMillis() - time + " ms");	
+	}
+	
+	public static void pairAPair(ArrayList<Rectangle> map){
+		Rectangle current = null;
+		int nb = 0;
+		int count = 0;
+		for(int i = 0; i < map.size(); i++){
+			current = map.get(i);
+			for(int j = i + 1 ; j < map.size(); j++){
+				count++;
+				if(current.intersectWith(map.get(j))){
+					//System.out.println("Intersection avec Rectagle " + i + " et " + j);
+					nb++;
+				}
+			}
+		}
+		System.out.println("Nombre de comparaisons : " + count + ", Intersections : " + nb);
 	}
 
+	public static void balayage(ArrayList<Rectangle> map){
+		Collections.sort(map);
+		int nb = 0;
+		Rectangle current = null;
+		Rectangle compared = null;
+		int count = 0;
+		for(int i = 0; i < map.size(); i++){
+			current = map.get(i);
+			for(int j = i + 1 ; j < map.size(); j++){
+				count++;
+				compared =  map.get(j);
+				/*
+				System.out.println("--------------------------------------------------------------");
+				System.out.println(current.getPointDroit().getX());
+				System.out.println(compared.getPointGauche().getX());
+				*/
+				if(current.getPointDroit().getX() < compared.getPointGauche().getX())
+					break;
+				if(current.intersectWith(compared)){
+					//System.out.println("Intersection avec Rectagle " + i + " et " + j);
+					nb++;
+				}
+			}
+		}
+		System.out.println("Nombre de comparaisons : " + count + ", Intersections : " + nb);
+	}
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,12 +1,45 @@
-public class Rectangle {
+public class Rectangle implements Comparable<Rectangle>{
 
-	private Point pointgauche;
-	private Point pointdroit;
+	private Point pointGauche;
+	private Point pointDroit;
 
-	public Rectangle(Point i, Point j) {
-		pointgauche = j;
-		pointdroit = i;
+	public Rectangle(Point gauche, Point droit) {
+		this.pointGauche = gauche;
+		this.pointDroit = droit;
 
+	}
+	
+	public boolean intersectWith(Rectangle r2) {
+		return (
+					( this.getPointGauche().getX() < r2.getPointGauche().getX() && this.getPointDroit().getX() > r2.getPointGauche().getX() )
+						||
+					(this.getPointGauche().getX() < r2.getPointDroit().getX() && this.getPointDroit().getX() > r2.getPointDroit().getX())
+				) && (
+					( this.getPointGauche().getY() < r2.getPointGauche().getY() && this.getPointDroit().getY() > r2.getPointGauche().getY() ) 
+						||
+					(this.getPointGauche().getY() < r2.getPointDroit().getY() && this.getPointDroit().getY() > r2.getPointDroit().getY())
+				);
+	}
+	
+	@Override
+	public int compareTo(Rectangle r2) {
+		return  this.getPointGauche().getX() - r2.getPointGauche().getX(); 
+	}
+
+	public Point getPointGauche() {
+		return pointGauche;
+	}
+
+	public void setPointGauche(Point pointGauche) {
+		this.pointGauche = pointGauche;
+	}
+
+	public Point getPointDroit() {
+		return pointDroit;
+	}
+
+	public void setPointDroit(Point pointDroit) {
+		this.pointDroit = pointDroit;
 	}
 
 }
